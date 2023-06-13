24696644c89ef464dde08483c9863e4d5502bbca
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-13 22:48:13 +0700

    Basics - Hello World
    Simple Timer State Machine

--- lib/aws-step-functions-workshop-stack.ts
+++ lib/aws-step-functions-workshop-stack.ts
@@ -1,6 +1,6 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as stepfunctions from "aws-cdk-lib/aws-stepfunctions";
 
 export class AwsStepFunctionsWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +12,16 @@ export class AwsStepFunctionsWorkshopStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsStepFunctionsWorkshopQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const timerStateMachine = new stepfunctions.StateMachine(
+      this,
+      "TimerStateMachine",
+      {
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        definition: new stepfunctions.Wait(this, "Wait for Timer", {
+          time: stepfunctions.WaitTime.secondsPath("$.timer_seconds"),
+        }).next(new stepfunctions.Succeed(this, "Success")),
+      }
+    );
   }
 }
